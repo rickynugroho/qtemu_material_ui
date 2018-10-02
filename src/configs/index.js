584bb4689459435643c759f346eec4789ed7a6d3
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-10-02 22:18:53 +0700

    Add redux persist config

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import rootReducer from '../reducers';
 
-// const store = createStore(rootReducer);
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const store = createStore(
   rootReducer,
